bc7a2a94da39b782da82de7745ddbe734e14cee5
Ch3lLIST4 <herohoang74@gmail.com>
2020-10-20 21:21:25 +0700

    Update dns.py

--- modules/recons/dns.py
+++ modules/recons/dns.py
@@ -11,57 +11,64 @@ Y = '\033[33m' # yellow
 
 def dnsrec(domain, output, data):
 	result = {}
-	print('\n\n' + Y + '[!]' + Y + ' Starting DNS Enumeration...' + W + '\n')
-	types = ['A', 'AAAA', 'ANY', 'CAA', 'CNAME', 'MX', 'NS', 'TXT']
-	full_ans = []
-	for Type in types:
-		q = dnslib.DNSRecord.question(domain, Type)
+
+	try:
+		print('\n\n' + Y + '[!]' + Y + ' Starting DNS Enumeration...' + W + '\n')
+		types = ['A', 'AAAA', 'ANY', 'CAA', 'CNAME', 'MX', 'NS', 'TXT']
+		full_ans = []
+		for Type in types:
+			q = dnslib.DNSRecord.question(domain, Type)
+			pkt = q.send('8.8.8.8', 53, tcp='UDP')
+			ans = dnslib.DNSRecord.parse(pkt)
+			ans = str(ans)
+			ans = ans.split('\n')
+			full_ans.extend(ans)
+		full_ans = set(full_ans)
+		dns_found = []
+
+		for entry in full_ans:
+			if entry.startswith(';') == False:
+				dns_found.append(entry)
+			else:
+				pass
+		
+		if len(dns_found) != 0:
+			for entry in dns_found:
+				print(G + '[+]' + C + ' {}'.format(entry) + W)
+				if output != 'None':
+					result.setdefault('dns', []).append(entry)
+		else:
+			print(R + '[-]' + C + ' DNS Records Not Found!' + W)
+			if output != 'None':
+				result.setdefault('dns', ['DNS Records Not Found'])
+		
+		dmarc_target = '_dmarc.' + domain
+		q = dnslib.DNSRecord.question(dmarc_target, 'TXT')
 		pkt = q.send('8.8.8.8', 53, tcp='UDP')
-		ans = dnslib.DNSRecord.parse(pkt)
-		ans = str(ans)
-		ans = ans.split('\n')
-		full_ans.extend(ans)
-	full_ans = set(full_ans)
-	dns_found = []
+		dmarc_ans = dnslib.DNSRecord.parse(pkt)
+		dmarc_ans = str(dmarc_ans)
+		dmarc_ans = dmarc_ans.split('\n')
+		dmarc_found = []
 
-	for entry in full_ans:
-		if entry.startswith(';') == False:
-			dns_found.append(entry)
+		for entry in dmarc_ans:
+			if entry.startswith('_dmarc') == True:
+				dmarc_found.append(entry)
+			else:
+				pass
+		if len(dmarc_found) != 0:
+			for entry in dmarc_found:
+				print(G + '[+]' + C + ' {}'.format(entry) + W)
+				if output != 'None':
+					result.setdefault('dmarc', []).append(entry)
 		else:
-			pass
-	
-	if len(dns_found) != 0:
-		for entry in dns_found:
-			print(G + '[+]' + C + ' {}'.format(entry) + W)
+			print('\n' + R + '[-]' + C + ' DMARC Record Not Found!' + W)
 			if output != 'None':
-				result.setdefault('dns', []).append(entry)
-	else:
-		print(R + '[-]' + C + ' DNS Records Not Found!' + W)
-		if output != 'None':
-			result.setdefault('dns', ['DNS Records Not Found'])
-	
-	dmarc_target = '_dmarc.' + domain
-	q = dnslib.DNSRecord.question(dmarc_target, 'TXT')
-	pkt = q.send('8.8.8.8', 53, tcp='UDP')
-	dmarc_ans = dnslib.DNSRecord.parse(pkt)
-	dmarc_ans = str(dmarc_ans)
-	dmarc_ans = dmarc_ans.split('\n')
-	dmarc_found = []
+				result.setdefault('dmarc', ['DMARC Record Not Found!'])
 
-	for entry in dmarc_ans:
-		if entry.startswith('_dmarc') == True:
-			dmarc_found.append(entry)
-		else:
-			pass
-	if len(dmarc_found) != 0:
-		for entry in dmarc_found:
-			print(G + '[+]' + C + ' {}'.format(entry) + W)
-			if output != 'None':
-				result.setdefault('dmarc', []).append(entry)
-	else:
-		print('\n' + R + '[-]' + C + ' DMARC Record Not Found!' + W)
-		if output != 'None':
-			result.setdefault('dmarc', ['DMARC Record Not Found!'])
+	except Exception as e:
+        print('\n' + R + '[-]' + C + ' Exception : ' + W + str(e) + '\n')
+        if output != 'None':
+            result.update({'Exception':str(e)})
 
 	if output != 'None':
 		dns_export(output, data, result)
