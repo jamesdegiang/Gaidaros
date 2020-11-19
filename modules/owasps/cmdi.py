c5aa7000d6c8ae10ef58a7c1e95c469f4c8f182e
jamesdegiang <38900765+jamesdegiang@users.noreply.github.com>
2020-11-20 00:27:47 +0700

    Update cmdi.py

--- modules/owasps/cmdi.py
+++ modules/owasps/cmdi.py
@@ -1,3 +1,4 @@
+
 import tldextract
 import argparse
 import sys
@@ -45,6 +46,9 @@ def get_form_details(form):
         details["method"] = method
         details["inputs"] = inputs
         return details
+    except AttributeError as N:
+        print(G + "[+]" + C + f" No action form detected on this site" + W)
+        return None
     except Exception as e:
         print('\n' + R + '[-] Exception : ' + C + str(e) + W)
 
@@ -96,15 +100,10 @@ def scan_cmdi(url, value_forms_malforms, cmdi_data):
         print(G + "[+]" + C + f" Detected {len(forms)} forms on {url}" + W)
         cmdi_data.append(f"Detected {len(forms)} forms on {url}")
         value_forms_malforms[0] = value_forms_malforms[0] + len(forms)
+        #os_script = "a | ping -c 2 127.0.0.1"
         #getpayload
         payload_path = './dictionary/payload.txt'
         f = open(payload_path,'r').readlines()
-#            for s in f:
-#                p = s.split(';')
-#                inc = p[0]
-#                outc = p[1]
-#            
-#        os_script = inc
         # returning value
         is_vulnerable = False
         # iterate over all forms
@@ -113,6 +112,7 @@ def scan_cmdi(url, value_forms_malforms, cmdi_data):
             inc = p[0]
             outc = p[1]
             os_script = inc
+            print('testttt: 'inc,'---',outc)
             for form in forms:
                 form_details = get_form_details(form)
                 if form_details == None:
@@ -127,7 +127,6 @@ def scan_cmdi(url, value_forms_malforms, cmdi_data):
                     value_forms_malforms[1] = value_forms_malforms[1] + 1
                     is_vulnerable = True
                     # won't break because we want to print other available vulnerable forms
-            
 
         if is_vulnerable == True:
             print(R + "[-]" + f" Command Injection detected on {url}" + W)
@@ -139,12 +138,13 @@ def scan_cmdi(url, value_forms_malforms, cmdi_data):
     except Exception as e:
         print(R + '[-] Exception : ' + C + str(e) + W)
 
+
 def cmdi(target, output, data):
     result = {}
     cmdi_data = []
 
     try:
-        print ('\n\n' + G + '[+]' + Y + ' Operating System Command Injection:' + W + '\n')
+        print ('\n\n' + G + '[+]' + Y + ' OS Command Injection (CMDi) :' + W + '\n')
 
         user_agent = {
             'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'
@@ -171,8 +171,11 @@ def cmdi(target, output, data):
             for link in links:
                 url = link.get('href')
                 if url != None:
-                    if domain in url:
-                        int_total.append(url)
+                    if not "http://" in url or "https://" in url:
+                        url = target +  "/" + url
+                    if not '#' in url:
+                        if domain in url:
+                            int_total.append(url)
 
             int_total = set(int_total)
 
@@ -191,7 +194,7 @@ def cmdi(target, output, data):
             print(R + '[-]' + C + ' Response code returned is not 200' + W)
 
         if output != 'None':
-            result['OS Command Injection'] = cmdi_data
+            result['CMDi'] = cmdi_data
 
     except Exception as e:
         print(R + '[-] Exception : ' + C + str(e) + W)
@@ -204,5 +207,4 @@ def cmdi(target, output, data):
 
 
 def cmdi_output(output, data, result):
-    data['module-Operating System Command Injection'] = result
-
+    data['module-OS Command Injection'] = result
